332792bf12ab1da249a0aabb023fc8be2d758e0e
gabrielhipolitoo <c.gabrielhipolitoh@gmail.com>
2024-06-04 19:35:21 -0300

    inital

---
+++ rotas/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+    return (
+        <h1>voce esta no about</h1>
+    )
+}
+
+export default About
---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { RouterProvider,createBrowserRouter } from 'react-router-dom';
+import DOMPurify from "dompurify";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import Home from "../rotas/home";
+import About from "../rotas/about";
+function App() {
+  const [data, setData] = useState();
+
+  // const teste = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       "http://localhost:10003/wp-json/wp/v2/portifolio/"
+  //     );
+  //     if (!response.ok) {
+  //       throw new Error(`Erro: ${response.status} ${response.statusText}`);
+  //     }
+  //     const data = await response.json();
+  //     setData(data);
+  //   } catch (error) {
+  //     console.error("Erro na requisição:", error);
+  //   }
+
+  //   { data &&
+  //     <div dangerouslySetInnerHTML={{__html: data['0']['portifolio']}}/>
+  //   }
+  // };
+
+
+  // useEffect(() => {
+  //   teste();
+  // }, []);
+
+
+  const router = createBrowserRouter([
+    {
+      path:"/home",
+      element:<Home/>
+    },
+    {
+      path:"/about",
+      element:<About/>
+    }
+  ])
+
+  return (
+    <RouterProvider router={router}/>
+
+  );
+}
+
+export default App;
